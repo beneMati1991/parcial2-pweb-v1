f897dfbb82856ed2169c4e1ff764a6f790e8e012
beneMati1991 <benedettomatias1991@gmail.com>
2021-06-19 15:01:54 -0300

    urls complete beginning project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { EpisodeComponent } from './pages/episode/episode.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'menu',
+    component: MenuComponent
+  },
+  {
+    path:'episode',
+    component: EpisodeComponent
+  },
+  {
+    path:'characters',
+    component: CharactersComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Import the module from the SDK.
+import { AuthModule } from '@auth0/auth0-angular';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { EpisodeComponent } from './pages/episode/episode.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    EpisodeComponent,
+    CharactersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // Import the module into the application, with configuration.
+    AuthModule.forRoot({
+      domain: 'dev-f5z9j2wu.us.auth0.com',
+      clientId: '82o1FfwPFSxvbaCxVlIuXzcUoogmuzlo'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
